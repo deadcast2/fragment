5a4fdc229922a00b1a4b8965b25bfaeb3aafc8b5
deadcast <deadcast@sent.com>
2018-07-08 13:20:25 -0700

    Removing dead code

--- audio.h
+++ audio.h
@@ -4,21 +4,12 @@
 #include "log.h"
 #include "XAudio2_custom.h"
 
-#define fourccRIFF 'FFIR'
-#define fourccDATA 'atad'
-#define fourccFMT ' tmf'
-#define fourccWAVE 'EVAW'
-
 IXAudio2 *xAudio2;
 IXAudio2MasteringVoice *xMasterVoice;
 IXAudio2SourceVoice *xSourceVoice;
 
 void InitAudio();
 void CleanAudio();
-HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD *dwChunkSize,
-  DWORD *dwChunkDataPosition);
-HRESULT ReadChunkData(HANDLE hFile, void *buffer, DWORD buffersize,
-  DWORD bufferoffset);
 void LoadTestAudio();
 
 #endif
